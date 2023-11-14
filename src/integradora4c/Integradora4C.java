12d7246612d4e03d7dcf87db4c9726edc1c44957
Juana Imelda De La Torre Ruiz <jdelatru@emeal.nttdata.com>
2023-11-14 08:46:42 -0600

    sin comentarios

---
+++ src/integradora4c/Integradora4C.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package integradora4c;
+
+/**
+ *
+ * @author jdelatru
+ */
+public class Integradora4C {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
